865d5257b3116f9a4f1ae35922bac78aefbebb7f
Vijay Rangan <vijay.rangan@gmail.com>
2018-04-28 13:47:59 +0530

    improve UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import "./App.css";
 import { client } from "./http";
-import React, { Component, Fragment } from "react";
+import classnames from "classnames";
 import Avatar from "material-ui/Avatar";
 import TextField from "material-ui/TextField";
+import React, { Component, Fragment } from "react";
 import CircularProgress from "material-ui/CircularProgress";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import {
@@ -52,37 +53,49 @@ class App extends Component {
       <MuiThemeProvider>
         <div className="container">
           <section className="search-bar">
-            <TextField hintText="Search Users" onChange={this.handleChange} />
+            <TextField
+              hintText="Search Users"
+              onChange={this.handleChange}
+              hintStyle={{
+                color: "rgba(255,255,255,0.4)",
+              }}
+              underlineStyle={{
+                borderColor: "rgba(255, 255, 255, 0.25)",
+              }}
+              underlineFocusStyle={{
+                borderColor: "#72DDF7",
+              }}
+            />
           </section>
           <Divider inset={true} />
           <section className="results">
             {fetching && <CircularProgress />}
-            {users.length > 0 && (
-              <Table selectable={false}>
-                <TableHeader displayRowCheckbox={false}>
-                  <TableRow>
-                    <TableHeaderColumn>#</TableHeaderColumn>
-                    <TableHeaderColumn>GitHub Username</TableHeaderColumn>
-                    <TableHeaderColumn>Profile Link</TableHeaderColumn>
-                  </TableRow>
-                </TableHeader>
-                <TableBody displayRowCheckbox={false}>
-                  {users.map((user, index) => {
-                    return (
-                      <TableRow key={user.login}>
-                        <TableRowColumn>{index + 1}</TableRowColumn>
-                        <TableRowColumn>{user.login}</TableRowColumn>
-                        <TableRowColumn>
-                          <a href={user.html_url} target="_blank">
-                            Visit GitHub Profile
-                          </a>
-                        </TableRowColumn>
-                      </TableRow>
-                    );
-                  })}
-                </TableBody>
-              </Table>
-            )}
+            <Table className={classnames("results-table", {
+              "results-table-show": users.length > 0
+            })} selectable={false}>
+              <TableHeader displayRowCheckbox={false}>
+                <TableRow>
+                  <TableHeaderColumn>#</TableHeaderColumn>
+                  <TableHeaderColumn>GitHub Username</TableHeaderColumn>
+                  <TableHeaderColumn>Profile Link</TableHeaderColumn>
+                </TableRow>
+              </TableHeader>
+              <TableBody displayRowCheckbox={false}>
+                {users.map((user, index) => {
+                  return (
+                    <TableRow key={user.login}>
+                      <TableRowColumn>{index + 1}</TableRowColumn>
+                      <TableRowColumn>{user.login}</TableRowColumn>
+                      <TableRowColumn>
+                        <a href={user.html_url} target="_blank">
+                          Visit GitHub Profile
+                        </a>
+                      </TableRowColumn>
+                    </TableRow>
+                  );
+                })}
+              </TableBody>
+            </Table>
           </section>
         </div>
       </MuiThemeProvider>
