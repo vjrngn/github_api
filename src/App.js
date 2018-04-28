d8f8db7fc2ea038c74cd1151a261b9cbf535dbf1
Vijay Rangan <vijay.rangan@gmail.com>
2018-04-28 11:31:34 +0530

    add material-ui

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import React, { Component } from "react";
 import "./App.css";
 import { client } from "./http";
+import React, { Component, Fragment } from "react";
+import Avatar from "material-ui/Avatar";
+import TextField from "material-ui/TextField";
+import CircularProgress from "material-ui/CircularProgress";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import {
+  Table,
+  TableBody,
+  TableHeader,
+  TableHeaderColumn,
+  TableRow,
+  TableRowColumn,
+} from "material-ui/Table";
+import { Divider } from "material-ui";
 
 class App extends Component {
   state = {
@@ -11,12 +24,14 @@ class App extends Component {
   handleChange = e => {
     const { value } = e.target;
 
+    if (value === "") {
+      this.setState({ users: [] });
+    }
+
     if (value !== "") {
       this.setState({ fetching: true }, () => {
         client
-          .get(
-            `/search/users?q=${value}&sort=followers&order=desc`
-          )
+          .get(`/search/users?q=${value}&sort=followers&order=desc`)
           .then(response => response.data.items)
           .then(users => {
             this.setState({ users, fetching: false });
@@ -34,40 +49,43 @@ class App extends Component {
     const { users, fetching } = this.state;
 
     return (
-      <div className="App">
-        <section className="search-bar">
-          <input type="text" onChange={this.handleChange} />
-        </section>
-        <section className="results">
-          {fetching && "Searching ..."}
-          {users.length > 0 && (
-            <table>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>GitHub Username</th>
-                  <th>Profile Link</th>
-                </tr>
-              </thead>
-              <tbody>
-                {users.map((user, index) => {
-                  return (
-                    <tr key={user.login}>
-                      <td>{index + 1}</td>
-                      <td>{user.login}</td>
-                      <td>
-                        <a href={user.html_url} target="_blank">
-                          Visit GitHub Profile
-                        </a>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          )}
-        </section>
-      </div>
+      <MuiThemeProvider>
+        <div className="container">
+          <section className="search-bar">
+            <TextField hintText="Search Users" onChange={this.handleChange} />
+          </section>
+          <Divider inset={true} />
+          <section className="results">
+            {fetching && <CircularProgress />}
+            {users.length > 0 && (
+              <Table selectable={false}>
+                <TableHeader displayRowCheckbox={false}>
+                  <TableRow>
+                    <TableHeaderColumn>#</TableHeaderColumn>
+                    <TableHeaderColumn>GitHub Username</TableHeaderColumn>
+                    <TableHeaderColumn>Profile Link</TableHeaderColumn>
+                  </TableRow>
+                </TableHeader>
+                <TableBody displayRowCheckbox={false}>
+                  {users.map((user, index) => {
+                    return (
+                      <TableRow key={user.login}>
+                        <TableRowColumn>{index + 1}</TableRowColumn>
+                        <TableRowColumn>{user.login}</TableRowColumn>
+                        <TableRowColumn>
+                          <a href={user.html_url} target="_blank">
+                            Visit GitHub Profile
+                          </a>
+                        </TableRowColumn>
+                      </TableRow>
+                    );
+                  })}
+                </TableBody>
+              </Table>
+            )}
+          </section>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
